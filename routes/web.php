a8db803a2d458638cd69ad1c671c89ce71d1f8ff
malvger <malvger@gmail.com>
2020-09-30 19:57:53 -0600

    Update web.php
    comit

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,7 @@ Route::resource('estudiantes1', 'Estudiantes1Controller');
 Route::patch('estudiantes1/{id}/edit', 'Estudiantes1eController@update')->name('estudiantes1.update');
 
 //route socioeconomico
+//comit 2
 
 Route::resource('estudiosocioeconimico', 'EstudioSocioeconimicoController');
 Route::patch('estudiosocioeconimico/{id}/edit', 'EstudioSocioeconimicoController@update')->name('estudiosocioeconimico.update');
