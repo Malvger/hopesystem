f23802b19f9bc64468b52dfbe9e8b65a87db52d8
Hugo Dónovan Aragón Cruz <58078215+DonovanAC7@users.noreply.github.com>
2020-10-13 19:55:18 -0600

    Creación de Formularios: Siituación de vivienda  y Salud física y Psicológica

--- resources/views/estudiosocioeconimico/formsfp.blade.php
+++ resources/views/estudiosocioeconimico/formsfp.blade.php
@@ -1,6 +1,143 @@
 <br>
+<h5>Salud Física</h5>
+<br>
+
+<div class="table-responsive">
+    <table class="table table-bordered" width="100%" cellspacing="0">
+        <thead>
+          <tr class="bg-secondary text-white">
+                <th scope="col">¿El estudiante sufre algún problema de salud?</th>
+        
+                <th scope="col">Especifique:</th>
+          </tr>
+
+          <tr>
+              <td>
+                    <input id="ProblemaSaludSi" type="checkbox" class="form-control" name="ProblemaSaludSi"
+                    {{isset($estudioS->ProblemaSaludSi)?$estudioS->ProblemaSaludSi=='1'?      'checked':'':'' }}
+                    value="1">
+              </td>
+    
+              <td>
+                    <input id="Especifique" type="text" class="form-control" name="Especifique"
+                    value="{{isset($estudioS->Especifique)?$estudioS->Especifique:'' }}"> 
+              </td>
+          </tr>
+        </thead>
+    
+        <thead>
+            <tr class="bg-secondary text-white">
+              <th scope="col">¿Algún miembro que conforma su hogar presenta un problema de salud?</th>
+              
+              <th scope="col">Especifique:</th>
+            </tr>
+
+            <tr>
+                <td>
+                  <input id="ProblemaSaludSi" type="checkbox" class="form-control" name="ProblemaSaludSi"
+                  {{isset($estudioS->ProblemaSaludSi)?$estudioS->ProblemaSaludSi=='1'?'checked':'':'' }}
+                    value="1">
+                </td>
+      
+                <td>
+                  <input id="Especifique" type="text" class="form-control" name="Especifique"
+                  value="{{isset($estudioS->Especifique)?$estudioS->Especifique:'' }}"> 
+                </td>
+            </tr>
+        </thead>
+    </table>
+</div>
+
+<br>
+<h5>Si la respuesta es sí, ¿a qué miembro que conforma su hogar afecta la enfermedad?</h5>
+<br>
+
+<div class="table-responsive">
+    <table class="table table-bordered" width="100%" cellspacing="0">
+        <thead>
+                <tr class="bg-secondary text-white">
+                  <th scope="col">Nombres y Apellidos:</th>
+                </tr>
+
+                <tr>
+                    <td>
+                        <input id="NombreApellido" type="text" class="form-control" name="NombreApellido"
+                        value="{{isset($estudioS->NombreApellido)?$estudioS->NombreApellido:'' }}"> 
+                    </td>
+                </tr>        
+        </thead>
+    </table>
+</div>
+    
+
+<br>
+<h5>Salud Psicológica</h5>
+<br>
+
+<div class="table-responsive">
+    <table class="table table-bordered" width="100%" cellspacing="0">
+        <thead>
+          <tr class="bg-secondary text-white"> <!--Filas-->
+            <th scope="col">¿El estudiante presenta algún tipo de discapacidad?</th> <!--Encabezados-->
+            <th scope="col">Especifique:</th>
+          <tr>
+              <td>
+                <input id="DiscapacidadSi" type="checkbox" class="form-control" name="DiscapacidadSi"
+                {{isset($estudioS->DiscapacidadSi)?$estudioS->DiscapacidadSi=='1'?'checked':'':'' }}
+                        value="1">
+              </td>
+    
+              <td>
+                <input id="Especifique" type="text" class="form-control" name="Especifique"
+                value="{{isset($estudioS->Especifique)?$estudioS->Especifique:'' }}"> 
+              </td>
+          </tr>
+        </thead>
+    
+        <thead>
+            <tr class="bg-secondary text-white">
+              <th scope="col">En caso de existir discapacidad, ¿Cuentan con algún certificado u otro documento que acredite la disccapacidad?</th>
+              
+              <th scope="col">De no contar con el certificado de discapacidad, señale el motivo</th>
+              
+            </tr>
+            <tr>
+                <td>
+                  <input id="CertificadoSi" type="checkbox" class="form-control" name="CertificadoSi"
+                  {{isset($estudioS->CertificadoSi)?$estudioS->CertificadoSi=='1'?'checked':'':'' }}
+                        value="1">
+                </td>
+      
+                <td>
+                  <input id="Motivo" type="text" class="form-control" name="Motivo"
+                  value="{{isset($estudioS->Motivo)?$estudioS->Motivo:'' }}"> 
+                </td>
+            </tr>
+          </thead>
+    </table>
+</div>
 
 
+<div class="table-responsive">
+    <table class="table table-bordered" width="100%" cellspacing="0">
+        <thead>
+            <thead>
+                <tr class="bg-secondary text-white"> 
+                  <th scope="col">Además del estudiante, ¿otro miembro de la familia presenta discapacidad?</th>
+                  
+                </tr>
+                <tr>
+                    <td>
+                        <input id="OtroDiscapacidadSi" type="checkbox" class="form-control" name="OtroDiscapacidadSi"
+                  {{isset($estudioS->OtroDiscapacidadSi)?$estudioS->OtroDiscapacidadSi=='1'?'checked':'':'' }}
+                        value="1">
+                    </td>
+        
+        </thead>
+    </table>
+</div>
+
+<br>
 
 <div class="row">
     <div class="col clearfix">
--- resources/views/estudiosocioeconimico/formsv.blade.php
+++ resources/views/estudiosocioeconimico/formsv.blade.php
@@ -1,5 +1,184 @@
 <br>
+<h5>La Vivienda que ocupa actualmente es:</h5>
+<br>
 
+<div class="table-responsive">
+    <table class="table table-bordered" width="100%" cellspacing="0">
+        <thead>
+          <tr class="bg-secondary text-white">
+            <th scope="col">Propia, totalmente pagada</th>
+            <th scope="col">Cedida por alguna institución</th>
+          <tr>
+              <td>
+                <input id="PropiaSi" type="checkbox" class="form-control" name="PropiaSi"
+                {{isset($estudioS->PropiaSi)?$estudioS->PropiaSi=='1'?'checked':'':'' }}
+                        value="1">
+              </td>
+    
+              <td>
+                <input id="CedidaSi" type="checkbox" class="form-control" name="CedidaSi"
+                {{isset($estudioS->CedidaSi)?$estudioS->CedidaSi=='1'?'checked':'':'' }}
+                        value="1">
+              </td>
+          </tr>
+        </thead>
+    
+        <thead>
+            <tr class="bg-secondary text-white"> 
+              <th scope="col">Prestada</th>
+              <th scope="col">Propia, pagándola a plazos</th>
+            </tr>
+
+            <tr>
+                <td>
+                  <input id="PrestadaSi" type="checkbox" class="form-control" name="PrestadaSi"
+                  {{isset($estudioS->PrestadaSi)?$estudioS->PrestadaSi=='1'?'checked':'':'' }}
+                        value="1">
+                </td>
+      
+                <td>
+                    <input id="PropiaSi" type="checkbox" class="form-control" name="PropiaSi"
+                    {{isset($estudioS->PropiaSi)?$estudioS->PropiaSi=='1'?'checked':'':'' }}
+                          value="1">
+                </td>
+            </tr>
+          </thead>
+
+          <thead>
+            <tr class="bg-secondary text-white">
+              <th scope="col">Alquilada</th>            
+              <th scope="col">Invasión</th>
+              
+            </tr>
+
+            <tr>
+                <td>
+                  <input id="AlquiladaSi" type="checkbox" class="form-control" name="AlquiladaSi"
+                  {{isset($estudioS->AlquiladaSi)?$estudioS->AlquiladaSi=='1'?'checked':'':'' }}
+                        value="1">
+                </td>
+      
+                <td>
+                    <input id="InvasionSi" type="checkbox" class="form-control" name="InvasionSi"
+                    {{isset($estudioS->InvasionSi)?$estudioS->InvasionSi=='1'?'checked':'':'' }}
+                          value="1">
+                </td>
+            </tr>
+          </thead>
+      </table>
+    </div>
+<br>
+
+<div class="table-responsive">
+    <table class="table table-bordered" width="100%" cellspacing="0">
+        <thead>
+            <thead>
+                <tr class="bg-secondary text-white"> 
+                  <th scope="col">Otros (Especificar)</th> 
+                </tr>
+
+                <tr>
+                    <td>
+                        <input id="Otros" type="text" class="form-control" name="Otros"
+                        value="{{isset($estudioS->Otros)?$estudioS->Otros:'' }}"> 
+                    </td>
+                </tr>
+        </thead>
+    </table>
+</div>
+
+<br>
+<h5>¿Cuántos ambientes tiene el hogar?</h5>
+<br>
+
+<div class="table-responsive">
+    <table class="table table-bordered" width="100%" cellspacing="0">
+        <thead>
+          <tr class="bg-secondary text-white"> <!--Filas-->
+            <th scope="col">Habitaciones</th> <!--Encabezados-->
+            <th scope="col">Comedor</th>
+          <tr>
+              <td>
+                <input id="Habitaciones" type="text" class="form-control" name="Habitaciones"
+                value="{{isset($estudioS->Habitaciones)?$estudioS->Habitaciones:'' }}"> 
+              </td>
+    
+              <td>
+                <input id="Comedor" type="text" class="form-control" name="Comedor"
+                        value="{{isset($estudioS->Comedor)?$estudioS->Comedor:'' }}"> 
+              </td>
+          </tr>
+        </thead>
+    
+        <thead>
+            <tr class="bg-secondary text-white">
+              <th scope="col">Cocina</th>  
+              <th scope="col">Lavandería</th> 
+            </tr>
+
+            <tr>
+                <td>
+                    <input id="Cocina" type="text" class="form-control" name="Cocina"
+                        value="{{isset($estudioS->Cocina)?$estudioS->Cocina:'' }}"> 
+                </td>
+      
+                <td>
+                    <input id="Lavanderia" type="text" class="form-control" name="Lavanderia"
+                    value="{{isset($estudioS->Lavanderia)?$estudioS->Lavanderia:'' }}"> 
+                </td>
+            </tr>
+          </thead>
+        </table>
+</div>
+
+
+<br>
+<h5>El hogar cuenta con un solo ambiente</h5>
+<br>
+
+<div class="table-responsive">
+    <table class="table table-bordered" width="100%" cellspacing="0">
+        <thead>
+          <tr class="bg-secondary text-white"> 
+            <th scope="col">Patio</th> 
+            <th scope="col">Sala</th>
+          <tr>
+              <td>
+                <input id="PatioSi" type="checkbox" class="form-control" name="PatioSi"
+                {{isset($estudioS->PatioSi)?$estudioS->PatioSi=='1'?'checked':'':'' }}
+                        value="1">
+              </td>
+    
+              <td>
+                <input id="SalaSi" type="checkbox" class="form-control" name="SalaSi"
+                {{isset($estudioS->SalaSi)?$estudioS->SalaSi=='1'?'checked':'':'' }}
+                        value="1">
+              </td>
+          </tr>
+        </thead>
+    
+        <thead>
+            <tr class="bg-secondary text-white"> 
+              <th scope="col">Baño</th> 
+              <th scope="col">Garaje</th>
+            </tr>
+
+            <tr>
+                <td>
+                  <input id="BanoSi" type="checkbox" class="form-control" name="BanoSi"
+                  {{isset($estudioS->BanoSi)?$estudioS->BanoSi=='1'?'checked':'':'' }}
+                        value="1">
+                </td>
+      
+                <td>
+                    <input id="GarajeSi" type="checkbox" class="form-control" name="GarajeSi"
+                    {{isset($estudioS->GarajeSi)?$estudioS->GarajeSi=='1'?'checked':'':'' }}
+                          value="1">
+                </td>
+            </tr>
+          </thead>
+        </table>
+</div>
 
 
 <div class="row">
