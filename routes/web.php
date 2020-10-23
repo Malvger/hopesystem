7be9213087e0543a4e3fc2ecd62bcb2a432e16f5
Hugo Dónovan Aragón Cruz <58078215+DonovanAC7@users.noreply.github.com>
2020-10-22 22:06:22 -0600

    Agregación de Reporte de Estudiantes

--- routes/web.php
+++ routes/web.php
@@ -27,10 +27,20 @@ Route::resource('estudiantes', 'EstudianteController');
 Route::patch('estudiantes/{id}/edit', 'EstudianteController@update')->name('estudiantes.update');
 
 Route::resource('estudiantes1', 'Estudiantes1Controller');
-Route::patch('estudiantes1/{id}/edit', 'Estudiantes1eController@update')->name('estudiantes1.update');
+Route::patch('estudiantes1/{id}/edit', 'Estudiantes1Controller@update')->name('estudiantes1.update');
 
 //route socioeconomico
 //comit 2
 
 Route::resource('estudiosocioeconimico', 'EstudioSocioeconimicoController');
 Route::patch('estudiosocioeconimico/{id}/edit', 'EstudioSocioeconimicoController@update')->name('estudiosocioeconimico.update');
+
+Route::resource('cursos', 'CursoController');
+Route::patch('cursos/{id}/edit', 'CursoController@update')->name('cursos.update'); 
+
+Route::resource('grados', 'GradoController');
+Route::patch('grados/{id}/edit', 'GradoController@update')->name('grados.update'); 
+Route::delete('grados/{id}', 'GradoController@destroy')->name('grados.destroy');
+
+
+Route::resource('reportes', 'ReportesController');
